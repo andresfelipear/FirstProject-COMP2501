10ab1275e0793abfd6f388e4c499e97a07a48f11
Andres Arevalo <andresfelipear@gmail.com>
2024-03-27 12:32:52 -0500

    working in main menu

--- src/code/Assignment2.java
+++ src/code/Assignment2.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Assignment2 of BCIT
@@ -27,6 +28,14 @@ public class Assignment2
     private static final int SQUARE_FOOTAGE = 3;
     private static final int CUSTOMER_PARKING = 4;
 
+    private static final int COMMERCIAL_PRICE_USD = 0;
+    private static final int COMMERCIAL_TYPE = 1;
+    private static final int COMMERCIAL_PROPERTY_ID = 2;
+    private static final int LOADING_DOCK = 3;
+    private static final int HIGHWAY_ACCESS = 4;
+
+    private static final int OPTION_GENERAL_QUERIES = 1;
+
 
     private final Agency agency;
 
@@ -36,12 +45,20 @@ public class Assignment2
     }
 
 
+    /**
+     * This method gets the ArrayList,<Address> and ArrayList<String> form AddressReader and PropertyReader,
+     * and uses them to create subtype Objects and adds them to the Agency.HashMap<String, Property> properties.
+     * @throws FileNotFoundException If addressFile or property file is not found throws and exception.
+     */
     public void init() throws FileNotFoundException
     {
         final ArrayList<Address> addresses;
         final ArrayList<String> stringProperties;
         final File addressesFile;
         final File propertiesFile;
+        Address address;
+        String propertyString;
+        Property property;
 
         addressesFile = new File(ADDRESSES_FILE_NAME);
         propertiesFile = new File(PROPERTIES_FILE_NAME);
@@ -49,8 +66,55 @@ public class Assignment2
         addresses = AddressReader.readAddressData(addressesFile);
         stringProperties = PropertyReader.readPropertyData(propertiesFile);
 
+        for(int i = 0; i < stringProperties.size(); i++)
+        {
+            propertyString = stringProperties.get(i);
+            address = addresses.get(i);
 
+            property = getPropertyFromString(propertyString, address);
+
+            agency.addProperty(property);
+        }
     }
+
+
+    public void doSearch()
+    {
+        Scanner scanner;
+        int mainMenuOption;
+        scanner = new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("Welcome to our Property search. ");
+            System.out.println("Choose one of the following options: ");
+            System.out.println("\t1. General Queries\n" +
+                                       "\t2. Residence Queries\n" +
+                                       "\t3. Commercial Queries\n" +
+                                       "\t4. Retail Queries\n" +
+                                       "\t5. Exit");
+
+            mainMenuOption = Integer.parseInt(scanner.next());
+
+            while(true)
+            {
+
+            }
+            if(mainMenuOption == OPTION_GENERAL_QUERIES)
+            {
+                System.out.println("General Queires");
+                System.out.println("\t1. By Property Id\n" +
+                                           "\t2. By Price\n" +
+                                           "\t3. By Street\n" +
+                                           "\t4. By Type\n" +
+                                           "\t5. Back");
+
+            }
+
+        }
+    }
+
+
 
     private static Property getPropertyFromString(final String propertyString, final Address address)
     {
@@ -71,20 +135,20 @@ public class Assignment2
         else if(propertyString.contains("retail"))
         {
             property = new Retail(Double.parseDouble(propertyArray[RETAIL_PRICE_USD]), address, propertyArray[RETAIL_TYPE],
-                                  propertyArray[RETAIL_PROPERTY_ID], propertyArray[SQUARE_FOOTAGE],
-                                  propertyArray[CUSTOMER_PARKING]);
+                                  propertyArray[RETAIL_PROPERTY_ID], Integer.parseInt(propertyArray[SQUARE_FOOTAGE]),
+                                  Boolean.parseBoolean(propertyArray[CUSTOMER_PARKING]));
         }
         else if(propertyString.contains("commercial"))
         {
-            property = new Commercial(propertyArray[COMMERCIAL_PRICE_USD], address, propertyArray[COMMERCIAL_TYPE],
-                                      propertyArray[COMMERCIAL_PROPERTY_ID], propertyArray[LOADING_DOCK],
-                                      propertyArray[HIGHWAY_ACCESS]);
+            property = new Commercial(Double.parseDouble(propertyArray[COMMERCIAL_PRICE_USD]), address,
+                                      propertyArray[COMMERCIAL_TYPE], propertyArray[COMMERCIAL_PROPERTY_ID],
+                                      Boolean.parseBoolean(propertyArray[LOADING_DOCK]), Boolean.parseBoolean(propertyArray[HIGHWAY_ACCESS]));
         }
         else
         {
             return null;
         }
 
-        property = new Property()
+        return property;
     }
 }
